32e409461c05e8ebe4992a728fc80cdd633cd3c4
freshiswhatwedo <117794962+freshiswhatwedo@users.noreply.github.com>
2024-03-19 16:06:03 -0400

    s

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import lyricsgenius
 
 token = "SJgV3xqS7Lim040mG6wG0e5pJAX6LrSZ4UIod4Dm392eBtALTyfVF18scCWbwMEP"
 genius = lyricsgenius.Genius(token)
-
+value = 0
 #artist = genius.search_artist("Andmax", max_songs=3, sort="title")
 #print(artist)
 #print("split")
