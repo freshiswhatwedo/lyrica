8faf028305dd6c07e0f5ee1eb9ac18f9ff87be0a
freshiswhatwedo <117794962+freshiswhatwedo@users.noreply.github.com>
2024-03-19 13:47:53 -0400

    s

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ token = "SJgV3xqS7Lim040mG6wG0e5pJAX6LrSZ4UIod4Dm392eBtALTyfVF18scCWbwMEP"
 genius = lyricsgenius.Genius(token)
 
 artist = genius.search_artist("Andmax", max_songs=3, sort="title")
+print(artist)
+print("")
 print(artist.songs)
 
-print("hi")
