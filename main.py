420af52a9006cb36b1e7a418ca05b9424579ab92
freshiswhatwedo <117794962+freshiswhatwedo@users.noreply.github.com>
2024-03-19 15:40:51 -0400

    io

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ def lyricFinder(Song,Artist):
     song = genius.search_song(Song, Artist)
     if song:
         lyrics_lines = song.lyrics.split('\n')
-        lyrics_dict = {i+1: line for i, line in enumerate(lyrics_lines) if line.strip() != ''}
+        filtered_lines = [line for line in lyrics_lines if not line.startswith('[') and line.strip() != '']
+        lyrics_dict = {i+1: line for i, line in enumerate(filtered_lines)}
         return lyrics_dict
     else:
         return None
