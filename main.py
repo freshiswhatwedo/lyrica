09095ad061a404202211335da8b313bd3e3878ab
freshiswhatwedo <117794962+freshiswhatwedo@users.noreply.github.com>
2024-03-19 13:23:19 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import lyricsgenius
+
+token = "SJgV3xqS7Lim040mG6wG0e5pJAX6LrSZ4UIod4Dm392eBtALTyfVF18scCWbwMEP"
+genius = lyricsgenius.Genius(token)
+
+artist = genius.search_artist("Andmax", max_songs=3, sort="title")
+print(artist.songs)
