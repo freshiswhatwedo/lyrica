7ef2ae6962c407ae2f0e0cb8ee4c0bc749457f74
freshiswhatwedo <117794962+freshiswhatwedo@users.noreply.github.com>
2024-03-19 13:53:11 -0400

    peep

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import lyricsgenius
 token = "SJgV3xqS7Lim040mG6wG0e5pJAX6LrSZ4UIod4Dm392eBtALTyfVF18scCWbwMEP"
 genius = lyricsgenius.Genius(token)
 
-artist = genius.search_artist("Andmax", max_songs=3, sort="title")
-print(artist)
-print("")
-print(artist.songs)
+##artist = genius.search_artist("Andmax", max_songs=3, sort="title")
+##print(artist)
+##print("split")
+##print(artist.songs)
 
+song = genius.search_song("haunt u", "lil peep")
+print(song.lyrics)
