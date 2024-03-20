83368cbf2be845680407a692b3da3480ba0e3370
Will <freshiswhatwedo123@gmail.com>
2024-03-20 00:29:05 -0400

    hey

--- main.py
+++ main.py
@@ -1,47 +1,7 @@
-import lyricsgenius
+import syncedlyrics
 
+song = "haunt u"
+artist= "lil peep"
+lrc = syncedlyrics.search(f"[{song}] [{artist}]")
 
-token = "SJgV3xqS7Lim040mG6wG0e5pJAX6LrSZ4UIod4Dm392eBtALTyfVF18scCWbwMEP"
-genius = lyricsgenius.Genius(token)
-#artist = genius.search_artist("Andmax", max_songs=3, sort="title")
-#print(artist)
-#print("split")
-#print(artist.songs)
-
-#song = genius.search_song("haunt u", "lil peep")
-#print(song.lyrics)
-
-def lyricFinder(Song,Artist):
-      song = genius.search_song(Song, Artist)
-      if song:
-        lyrics_lines = song.lyrics.split('\n')
-        lyrics_dict = {i+1: line for i, line in enumerate(lyrics_lines) if line.strip() != ''}
-        return lyrics_dict
-      
-
-def lineNumber(L):
-    for i in L:
-        value = 0
-        value = value + 1
-        return value
-
-
-# Song = input("Whats the songs name?")
-# Artist = input("Whats the artists name?")
-  
-Song = "beamer boy"
-Artist = "lil peep"
-
-Lyrics = lyricFinder(Song,Artist)
-linenumber = lineNumber(Lyrics)
-print(linenumber)
-print(Lyrics[3])
-for i in range(2,10):
-    print(i)
-
-
-
-# for i in Lyrics[range(2,lineNumber)]
-# print(Lyrics)
-   
-
+print(lrc)
